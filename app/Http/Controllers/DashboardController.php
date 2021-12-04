4f755b5d4ee611785571de8c737d9fcfa4535dd6
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2021-12-04 03:18:36 +0200

    add chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Connectoin;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,9 +12,23 @@ class DashboardController extends Controller
     public function index(){
 
         $number_database = DB::table('connection')->count();
-        
+        $record= DB::table('connection')->select(DB::raw("COUNT(*) as count"), DB::raw("DATE(created_at) as date_name"), DB::raw("date(created_at) as date"))
+        ->where('created_at', '>', Carbon::today()->subDay(10))
+        ->groupBy('date_name','date')
+        ->orderBy('date')
+        ->get();
+  
+        $data = [];
+ 
+        foreach($record as $row) {
+           $data['label'][] = $row->date_name;
+           $data['data'][] = (int) $row->count;
+         }
+    
+       $data['chart_data'] = json_encode($data);
         return view('super-db.dashboard',[
-            'number_database' => $number_database
+            'number_database' => $number_database,
+            'chart_data'=>$data['chart_data']
         ]);
     }
 }
