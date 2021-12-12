26e949fe395653e1c45f36e059bcfec063371226
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2021-12-12 02:30:09 +0200

    edit logo

--- storage/framework/views/ece1f2be95a836fe9f0430cbc4cc6e596d18fdf8.php
+++ storage/framework/views/ece1f2be95a836fe9f0430cbc4cc6e596d18fdf8.php
@@ -35,7 +35,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top" style=" color: #CEF0D4; font-family: 'Brush Script MT', Brush Script Std, cursive	; font-size: 20px; text-align: center; text-shadow: 1px 1px 10px #ffffff;">Super DB</a>
+                <a class="navbar-brand glitch" href="#page-top"data-text="Super DB" style=" color: #CEF0D4; font-family: 'Brush Script MT', Brush Script Std, cursive	; font-size: 20px; text-align: center; text-shadow: 1px 1px 10px #ffffff;">Super DB</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
